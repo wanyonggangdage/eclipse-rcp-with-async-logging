6486343287d71828e90d9c269a347f376afeb5cd
Stephan Druskat <stephan.druskat@uni-jena.de>
2015-10-27 12:06:09 +0100

    Add logging plugin and log4j2.xml

--- net.sdruskat.blog.rcp.asynclog4j2/src/main/java/net/sdruskat/blog/rcp/asynclog4j2/Application.java
+++ net.sdruskat.blog.rcp.asynclog4j2/src/main/java/net/sdruskat/blog/rcp/asynclog4j2/Application.java
@@ -1,5 +1,7 @@
 package net.sdruskat.blog.rcp.asynclog4j2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
@@ -10,12 +12,17 @@ import org.eclipse.ui.PlatformUI;
  * This class controls all aspects of the application's execution
  */
 public class Application implements IApplication {
+	
+	private static final Logger logger = LogManager.getLogger(Application.class);
 
 	/* (non-Javadoc)
 	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
 	 */
 	public Object start(IApplicationContext context) throws Exception {
 		Display display = PlatformUI.createDisplay();
+		
+		logger.error("Works!");
+		
 		try {
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			if (returnCode == PlatformUI.RETURN_RESTART)
